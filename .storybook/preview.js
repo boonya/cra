4204888cfb5457508c930231645e1bf4c23ea7f6
Serhii [boonya] Buinytskyi <779184+boonya@users.noreply.github.com>
2023-03-10 18:22:16 +0100

    Bump dependencies

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,11 +1,18 @@
+import {initialize, mswDecorator} from 'msw-storybook-addon';
 import withRouter from '@sb/decorators/withRouter';
 import withStyle from '@sb/decorators/withStyle';
 import withTheme, {getThemeToolbar} from '@sb/decorators/withTheme';
+
+initialize({
+	serviceWorker: {url: `${APP_PREFIX}mockServiceWorker.js`},
+	onUnhandledRequest: 'bypass',
+});
 
 export const decorators = [
 	withStyle(),
 	withTheme(),
 	withRouter(),
+	mswDecorator,
 ];
 
 export const parameters = {
